b463013e46b4357c278cb637c051c63795064cbe
Matt Way <matt@syntropy.xyz>
2018-09-21 22:56:06 +1000

    refactored destyle to work with state as well as props

--- src/destyle.js
+++ src/destyle.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 const defaultConcatenator = styleList => styleList.join(' ')
 
@@ -12,34 +12,6 @@ const styles = {}
 
 const setConcatenator = con => (config.concatenator = con)
 
-const styleFunc = (names, props) => {
-  // create merged value object
-  const merged = names.reduce((obj, name) => {
-    const item = styles[name] || {}
-    Object.keys(item).forEach(k => {
-      obj[k] = obj[k] || []
-      obj[k] = obj[k].concat(item[k])
-    })
-    return obj
-  }, {})
-
-  // replace the value array with a prop getter
-  return Object.keys(merged).reduce((obj, key) => {
-    return Object.defineProperty(obj, key, {
-      get: function() {
-        const styleList = merged[key].map(style => {
-          if (typeof style === 'function') {
-            return style(props, this)
-          } else {
-            return style
-          }
-        })
-        return config.concatenator(styleList)
-      }
-    })
-  }, {})
-}
-
 const getDisplayName = (
   WrappedComponent,
   defaultName = 'Component'
@@ -52,22 +24,88 @@ const getDisplayName = (
   )
 }
 
+const isStateless = component => {
+  const proto = component.prototype || {}
+
+  return (
+    !component.isReactComponent &&
+    !proto.isReactComponent &&
+    !component.render &&
+    !proto.render
+  )
+}
+
 const destyle = (TheComponent, name) => {
-  const HOCName = `destyle(${getDisplayName(
-    TheComponent,
-    name
-  )})`
+  const compName = getDisplayName(TheComponent, name)
+  const HOCName = `destyle(${compName})`
 
-  const HOC = ({ destyleNames = '', ...rest }) => {
-    const names = destyleNames.split(' ')
-    names.unshift(name)
-
-    return (
-      <TheComponent
-        styles={styleFunc(names, rest)}
-        {...rest}
-      />
+  if (!name) {
+    console.warn(
+      `destyle was applied to ${compName} without providing a namespace.`
     )
+  }
+
+  if (isStateless(TheComponent)) {
+    const component = TheComponent
+    TheComponent = class extends Component {
+      render() {
+        return component(this.props, this.context)
+      }
+    }
+  }
+
+  class InnerComponent extends TheComponent {
+    styleFunc = (stylesObj, names) => {
+      // create merged value object
+      const merged = names.reduce((obj, name) => {
+        const item = styles[name] || {}
+        Object.keys(item).forEach(k => {
+          obj[k] = obj[k] || []
+          obj[k] = obj[k].concat(item[k])
+        })
+        return obj
+      }, {})
+
+      // replace the value array with a prop getter
+      Object.keys(merged).forEach(key => {
+        Object.defineProperty(stylesObj, key, {
+          configurable: true,
+          get: () => {
+            const styleList = merged[key].map(style => {
+              if (typeof style === 'function') {
+                return style(this.props, this.state)
+              } else {
+                return style
+              }
+            })
+            return config.concatenator(styleList)
+          }
+        })
+      })
+    }
+
+    render() {
+      const { destyleNames } = this.props
+      const names = destyleNames
+        ? destyleNames.split(' ')
+        : []
+      if (name) {
+        names.unshift(name)
+      }
+
+      // modify the styles object passed to the wrapped render
+      if (names.length > 0) {
+        this.styleFunc(this.props.styles, names)
+      }
+
+      return super.render()
+    }
+  }
+
+  class HOC extends Component {
+    render() {
+      return <InnerComponent styles={{}} {...this.props} />
+    }
   }
 
   HOC.displayName = HOCName
