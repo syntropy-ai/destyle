817edeee183883d8cd0692ae9039c6179dc0a8d2
matt-way <matt@syntropy.xyz>
2019-06-15 10:13:05 +1000

    simplified build

--- rollup.config.js
+++ rollup.config.js
@@ -1,60 +1,20 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
-import { uglify } from 'rollup-plugin-uglify'
 
-const globals = {
-  react: 'React'
-}
-
-const env = process.env.NODE_ENV
 const config = {
   input: 'src/destyle.js',
-  plugins: [],
-  external: ['react']
-}
-
-if (env === 'es' || env === 'cjs') {
-  config.output = { globals, format: env, indent: false }
-  config.plugins.push(
-    babel({
-      plugins: ['external-helpers']
-    })
-  )
-}
-
-if (env === 'development' || env === 'production') {
-  config.output = {
-    globals,
+  external: ['react'],
+  output: {
     format: 'umd',
     name: 'destyle',
-    indent: false
-  }
-  config.plugins.push(
-    nodeResolve({
-      jsnext: true
-    }),
+    globals: {
+      react: 'React'
+    }
+  },
+  plugins: [
     babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers']
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(env)
+      exclude: 'node_modules/**'
     })
-  )
-}
-
-if (env === 'production') {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  )
+  ]
 }
 
 export default config
